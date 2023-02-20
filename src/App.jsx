60e90ab8de2fafc61dc823d51d5694e96da48b6f
song <55rsearchco@gmail.com>
2023-02-20 13:15:58 +0900

    Styled Components 사용해보기
    Styled Components 사용해보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Button1 from './components/Button1';
 import Button2 from './components/Button2';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Button1 />
       <Button2 />
-    </div>
+      <StyledComponent />
+    </>
   );
 }
 
